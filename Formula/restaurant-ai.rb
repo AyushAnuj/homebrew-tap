e0b69671fa6c6a8c93030b1a8d90052bcfc93784
Ayushkrity Anuj <ayushkrityanuj@Ayushkritys-MacBook-Air.local>
2026-02-21 14:30:38 +0530

    update the virtual env creation

--- Formula/restaurant-ai.rb
+++ Formula/restaurant-ai.rb
@@ -3,23 +3,26 @@ class RestaurantAi < Formula
 
   desc "AI Restaurant Generator using Streamlit and Ollama"
   homepage "https://github.com/AyushAnuj/resturant-ai"
-
   url "https://github.com/AyushAnuj/resturant-ai/archive/refs/tags/v1.0.0.tar.gz"
-
   sha256 "8dc242b22a0519bb13681bf83e7de7b97a254f50261c3964f61ea02a75fa142b"
-
   license "MIT"
 
   depends_on "python@3.11"
   depends_on "ollama"
 
   def install
-    virtualenv_install_with_resources
+    virtualenv_create(libexec, "python3.11")
+
+    system libexec/"bin/pip", "install", "."
+
+    system libexec/"bin/pip", "install", "-r", "requirements.txt"
+
+    bin.install_symlink libexec/"bin/resturant-ai"
   end
 
   def caveats
     <<~EOS
-      Run the app with:
+      Run:
 
         resturant-ai
 
@@ -28,5 +31,4 @@ class RestaurantAi < Formula
         ollama serve
     EOS
   end
-
 end
