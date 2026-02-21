cb9570b84c92c86b275f926a9b143b414fb42d40
Ayushkrity Anuj <ayushkrityanuj@Ayushkritys-MacBook-Air.local>
2026-02-21 15:14:45 +0530

    commit 1

--- Formula/resturant-ai.rb
+++ Formula/resturant-ai.rb
@@ -12,11 +12,13 @@ class ResturantAi < Formula
 
   def install
     venv = virtualenv_create(libexec, "python3.11")
-    venv.pip_install_and_link buildpath
-    venv.pip_install_and_link "streamlit"
-    venv.pip_install_and_link "langchain-core"
-    venv.pip_install_and_link "langchain-community"
-    venv.pip_install_and_link "langchain-ollama"
+    venv.pip_install "streamlit"
+    venv.pip_install "langchain-core"
+    venv.pip_install "langchain-community"
+    venv.pip_install "langchain-ollama"
+    venv.pip_install buildpath
+    # Link CLI manually
+    bin.install_symlink libexec/"bin/resturant-ai"
   end
 
   def caveats
